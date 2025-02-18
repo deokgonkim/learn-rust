931d2e5ef80caa4452c2bb88f6816a6d7430c3d3
Deokgon Kim <dgkim@dgkim.net>
2025-02-18 15:45:03 +0900

    doc: add `documentation comment` on exercices/minigrep

--- exercises/minigrep/src/lib.rs
+++ exercises/minigrep/src/lib.rs
@@ -1,8 +1,16 @@
+//! This is minigrep
+//!
+//! `minigrep` is a program similar to `grep` on unix
+
 // use std::io;
 // use std::io::Error;
 use std::fs::File;
 use std::io::Read;
 
+/// Configuration for running this program
+///
+/// - file_path: `String` file path
+/// - text_to_find: `String` the string to be searched on each line of the file
 #[derive(Debug)]
 pub struct Config {
     file_path: String,
@@ -10,6 +18,14 @@ pub struct Config {
 }
 
 impl Config {
+
+    /// Returns `Config` struct that contains `file_path` and `text_to_find`
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// let config = Config::build(env::args().collect());
+    /// ```
     pub fn build(args: &Vec<String>) -> Result<Self, &'static str> {
         if args.len() < 2 {
             return Err("Not enough arguments");
@@ -22,6 +38,11 @@ impl Config {
     }
 }
 
+/// Runs main functionality
+///
+/// # Error
+/// - When there is a error opening file
+/// - When there is a problem to read a file
 pub fn run(config: &Config) -> Result<(), &str> {
     dbg!("Start Run");
     let file = File::open(&config.file_path);
