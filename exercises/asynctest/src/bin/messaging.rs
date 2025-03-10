2a58bab93b932d9c5889fe80717ac07b92a9cdcd
Deokgon Kim <dgkim@dgkim.net>
2025-03-10 15:58:41 +0900

    til: 20250310 async messaging
    Tested async messaging with `trpl::channel`

---
+++ exercises/asynctest/src/bin/messaging.rs
@@ -0,0 +1,28 @@
+use trpl;
+use core::time::Duration;
+
+
+fn main() {
+    let (tx, mut rx) = trpl::channel();
+    let future0 = async {
+        let future = async move {
+            println!("Begun Future1");
+            trpl::sleep(Duration::from_secs(5)).await;
+            let _ = tx.send("Hello");
+        };
+
+        let future2 = async move {
+            println!("Begun Future2");
+            while let Some(value) = rx.recv().await {
+                println!("Received {}", value);
+            }
+        };
+        trpl::join(future, future2).await;
+        // future.await;
+        // future2.await;
+    };
+    trpl::run(
+        future0
+    );
+}
+
